0ac529ab74ceb3ed25fa0a9bc57c9f605eef77fa
AlexNick03 <73990514+AlexNick03@users.noreply.github.com>
2025-06-16 02:33:23 +0300

    Ready for deployment on Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gymshop.wsgi:application
--- gymshop/settings.py
+++ gymshop/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS = ['gymshop.onrender.com', 'localhost']
 
 # Application definition
 
